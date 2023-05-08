4df31738bb88de181b98813618880dd7fb4f157a
abbv75 <bore.younous59@gmail.com>
2023-05-08 17:49:14 +0000

    ajoue de font awesome

--- ista/list/etudiant.php
+++ ista/list/etudiant.php
@@ -9,23 +9,25 @@
     <link rel="stylesheet" href="../css/list.css">
 </head>
 <body>
-    <table >
+    <table style="border: 1px solid; border-collapse: collapse;">
         <thead>
-            <th>Id</th>
+            <th>Num</th>
             <th>Nom</th>
             <th>Prenom</th>
             <th>Classe</th>
             <th>Telephone</th>
+            <th>Action</th>
         </thead>
         <tbody>
             <?php
                 require("../connecteDb.php");
                 $query = $bdd->query('SELECT * FROM etudiant ORDER BY nom');
+                $i=0;
                 while($res = $query->fetch()){
             ?>
                     <tr>
                         <td>
-                            <?php echo($res['ide']) ?>
+                            <?php echo($i++) ?>
                         </td>
                         <td>
                             <?php echo($res['nom']) ?>
@@ -38,6 +40,9 @@
                         </td>
                         <td>
                             <?php echo($res['telephone']) ?>
+                        </td>
+                        <td>
+
                         </td>
                     </tr>
             <?php
